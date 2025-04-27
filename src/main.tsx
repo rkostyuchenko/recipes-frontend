a282280800a917395e8557d56c9dfaf6870142fa
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2025-04-27 14:48:39 +0300

    use hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { HashRouter } from 'react-router';
 import AppRoot from 'components/app-root';
 
 import './stores/config';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppRoot />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
